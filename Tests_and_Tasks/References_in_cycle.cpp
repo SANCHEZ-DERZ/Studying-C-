2020d0ca9d652878e47723adbe8443a57b3ee28f
Alexander <derznovenko2005@mail.ru>
2023-11-07 22:53:06 +0300

    Changed References_in_cycle

--- Tests_and_Tasks/References_in_cycle.cpp
+++ Tests_and_Tasks/References_in_cycle.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
+
 int main() {
-	std::vector<int> a = { 1, 2, 3, 4, 5, 6 };
-	for (int elem : a) {
-		std::cout << elem << " ";
-	}
+    // создаём вектор из m строк длины n
+    // и искусственно заполняем его:
+    const size_t m = 1000000;
+    const size_t n = 10000;
+    std::vector<std::string> v(m);
+    for (size_t i = 0; i != m; ++i) {
+        v[i].resize(n, '@');  // кладём в вектор строку из n символов @
+    }
+
+    // нам интересен этот цикл:
+    size_t sum = 0;
+    for (const auto& row : v) {
+        sum += row.size();
+    }
+    std::cout << sum << "\n";
 }
