4966648f66b759ddc9089664ae1e5d93ca7a33a7
Alexander <derznovenko2005@mail.ru>
2023-11-07 22:27:52 +0300

    Added References_in_cycle

--- Tests_and_Tasks/References_in_cycle.cpp
+++ Tests_and_Tasks/References_in_cycle.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 int main() {
-	std::vector<int> a = { 1, 2, 3, 4, 5 };
+	std::vector<int> a = { 1, 2, 3, 4, 5, 6 };
 	for (int elem : a) {
 		std::cout << elem << " ";
 	}
