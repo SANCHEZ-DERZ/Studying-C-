0c9160f523a221528e28f3f8ca16d53f05fa061b
Alexander <derznovenko2005@mail.ru>
2023-11-07 20:42:21 +0300

    Changed Pointer

--- Tests_and_Tasks/Pointer.cpp
+++ Tests_and_Tasks/Pointer.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+struct Point {
+	double x, y, z;
+};
 int main() {
 	int x = 42;
-	int* ptr = &x; //Сохраняем адрес в памяти переменной х в указатель ptr
+	int* ptr1 = &x; //Сохраняем адрес в памяти переменной х в указатель ptr
 
 	++x;
-	std::cout << *ptr << "\n";//43
-	std::cout << ptr << "\n";//Адрес переменной х
+	std::cout << *ptr1 << "\n";//43
+	std::cout << ptr1 << "\n";//Адрес переменной х
+
+	Point p = { .x = 3.0, .y = 4.0, .z = 5.0 };
+
+	Point* ptr2 = &p;
+
+	std::cout << (*ptr2).x << "\n"; // Обращение через * и . требует скобок
+	std::cout << ptr2->x << "\n";// то же самое, но чуть короче
 }
