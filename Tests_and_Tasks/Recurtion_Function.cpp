d6dc42201bbf5f6e1be24ffc1351ff6b9b3109b9
Alexander <derznovenko2005@mail.ru>
2023-11-10 22:14:11 +0300

    Addded Recurtion_Fuction

---
+++ Tests_and_Tasks/Recurtion_Function.cpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+#include <iostream>
+
+std::uint64_t Factorial(std::uint64_t n) {
+	if (n == 0) {
+		return 1;
+	}
+	return n * Factorial(n - 1); // Рекурсивный вызов
+}
+
+int main() {
+	std::cout << Factorial(5) << "\n"; // 120
+}
