993d8a5db7262c336436bb994213403499c27fe4
Alexander <derznovenko2005@mail.ru>
2023-11-11 00:49:19 +0300

    Added LambdaFuction

---
+++ Tests_and_Tasks/LambdaFunction.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <vector>
+
+struct Date {
+    int year, month, day;
+};
+
+int main() {
+    std::vector<Date> dates;
+    std::sort(dates.begin(), dates.end(), [](const Date& lhs, const Date& rhs) {
+        return std::tie(lhs.month, lhs.day) < std::tie(rhs.month, rhs.day);
+        });
+}
