e8429f5dbf371cbcabf68600b60b7a13caeb7c0b
Alexander <derznovenko2005@mail.ru>
2023-11-27 22:35:25 +0300

    Added OverloadingTemplateFunction

---
+++ Tests_and_Tasks/OverloadingTemplateFunctions.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+
+// общая версия
+template <typename T>
+T Max(const T& x, const T& y) {
+    if (x > y) {
+        return x;
+    }
+    else {
+        return y;
+    }
+}
+
+
+// перегрузка для векторов
+template <typename T>
+const std::vector<T>& Max(const std::vector<T>& v1, const std::vector<T>& v2) {
+    if (v1.size() > v2.size()) {
+        return v1;
+    }
+    else if (v1.size() < v2.size()) {
+        return v2;
+    }
+    else if (v1 > v2) {
+        return v1;
+    }
+    else {
+        return v2;
+    }
+}
+
+
+int main() {
+    std::cout << Max(1, 2) << "\n"; // вызов общей версии
+    std::vector<int> v1 = { 1, 2, 3 };
+    std::vector<int> v2 = { 4, 5 };
+    for (int x : Max(v1, v2)) {
+        std::cout << x << " "; // 1 2 3
+    }
+    std::cout << "\n";
+}
